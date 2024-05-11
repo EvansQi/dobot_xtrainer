c9a9f72f9291646fb0623b8fe3b51201ff511e00
Yiliang <942529748@qq.com>
2024-05-11 18:00:22 +0800

    1.修改部分注释为英文；2.toltal_time参数初始化

--- ModelTrain/model_inference_test.py
+++ ModelTrain/model_inference_test.py
@@ -8,13 +8,12 @@ if __name__ == '__main__':
     observation = {'qpos':[],'images':{'left_wrist':[],'right_wrist':[],'top':[]}}
     i=0
     while i<10:
-        observation['qpos'] = [0., -0.96, 1.16, 0., -0.3, 0., 0.09984833, 0., -0.96, 1.16, 0., -0.3, 0.,
-                               0.09984833]  # 注意调试获取pos_numpy时，显示的qpos_numpy正负号可能不对，因此需要用qpos_numpy[i]来显示
-        observation['images']['left_wrist'] = cv2.imread("./testimg/left_wrist.jpg", 1)
+        observation['qpos'] = [-1.57, 0, -1.57, 0, 1.57, 1.57, 1, 1.57, 0, 1.57, 0, -1.57, -1.57, 1]  #  input joint value (unit radians) and Grippers value(0~1).The 7th and 14th values are the left and right hand gripper values, respectively
+        observation['images']['left_wrist'] = cv2.imread("./testimg/left_wrist.jpg", 1)  # input image
         observation['images']['right_wrist'] = cv2.imread("./testimg/right_wrist.jpg", 1)
         observation['images']['top'] = cv2.imread("./testimg/top.jpg", 1)
-        cv2.imshow("11", observation['images']['left_wrist'])
+        cv2.imshow("img", observation['images']['left_wrist'])
         cv2.waitKey(10)
-        action = model.predict(observation,i)
+        action = model.predict(observation,i)  # out put
         print(action)
         i +=1
