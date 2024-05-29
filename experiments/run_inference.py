bfb46299b065d6a100abd9630022eb670a661b58
蔡同彪 <caitongbiao@dobot.cc>
2024-05-29 17:52:53 +0800

    fix bug: inference camera

--- experiments/run_inference.py
+++ experiments/run_inference.py
@@ -12,6 +12,8 @@ from dobot_control.env import RobotEnv
 from dobot_control.robots.robot_node import ZMQClientRobot
 from dobot_control.cameras.realsense_camera import RealSenseCamera
 
+from scripts.manipulate_utils import load_ini_data_camera
+
 from ModelTrain.module.model_module import Imitate_Model
 
 @dataclass
@@ -34,14 +36,14 @@ def run_thread_cam(rs_cam, which_cam):
         while thread_run:
             time.sleep(0.002)
             with lock:
-                image_top, _ = rs_cam.read()
-                image_top = image_top[:, :, ::-1]
+                image_right, _ = rs_cam.read()
+                image_right = image_right[:, :, ::-1]
     elif which_cam==2:
         while thread_run:
             time.sleep(0.002)
             with lock:
-                image_right, _ = rs_cam.read()
-                image_right = image_right[:, :, ::-1]
+                image_top, _ = rs_cam.read()
+                image_top = image_top[:, :, ::-1]
     else:
         print("Camera index error! ")
 
@@ -51,13 +53,13 @@ def main(args):
    # camera init
     global image_left, image_right, image_top, thread_run
     thread_run=True
-    rs1 = RealSenseCamera(flip=False, device_id="130322270390")
-    rs2 = RealSenseCamera(flip=True, device_id="130322272313")
-    rs3 = RealSenseCamera(flip=True, device_id="130322272737")
-    # rs1 = RealSenseCamera(flip=True, device_id="130322273839")
-    # rs2 = RealSenseCamera(flip=False, device_id="130322272591")
-    # rs3 = RealSenseCamera(flip=True, device_id="130322272429")
-
+    camera_dict = load_ini_data_camera()
+    rs1 = RealSenseCamera(flip=False, device_id=camera_dict["left"])
+    rs2 = RealSenseCamera(flip=True, device_id=camera_dict["right"])
+    rs3 = RealSenseCamera(flip=True, device_id=camera_dict["top"])
+    # rs1 = RealSenseCamera(flip=False, device_id="130322270390")
+    # rs2 = RealSenseCamera(flip=True, device_id="130322272313")
+    # rs3 = RealSenseCamera(flip=True, device_id="130322272737")
     thread_cam_left = threading.Thread(target=run_thread_cam, args=(rs1, 0))
     thread_cam_right = threading.Thread(target=run_thread_cam, args=(rs2, 1))
     thread_cam_top = threading.Thread(target=run_thread_cam, args=(rs3, 2))
